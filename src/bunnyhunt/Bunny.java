09be68a922dad67eb2f8b2f9887a9f5fba53ede1
avinanick <avinanick@gmail.com>
2015-10-29 13:00:32 -0700

    Added behavior for the bunny to avoid the wolf

--- src/bunnyhunt/Bunny.java
+++ src/bunnyhunt/Bunny.java
@@ -15,6 +15,7 @@ public class Bunny extends Animal {
     
     @Override
     int decideMove() {
+        int currentDirection = random(Model.MIN_DIRECTION, Model.MAX_DIRECTION);
         //Much of this uses similar code to wolf
         //Refactoring would likely put a lot of this functionality in Animal class
         canSeeWolfNow = false;
@@ -29,10 +30,24 @@ public class Bunny extends Animal {
         
         //If the wolf was seen, move out of its line of site
         //The preferred directions away will be direction to wolf + 4 +or- 1
+        //If the bunny cannot move in any of the preferred directions, it will pick a random direction it can move
+        //unless it is stuck
         if(this.canSeeWolfNow) {
-            
+            int tries = 0;
+            int prefDirection1 = (this.directionToWolf + 3) % (Model.MAX_DIRECTION + 1);
+            int prefDirection2 = (this.directionToWolf + 5) % (Model.MAX_DIRECTION + 1);
+            if (canMove(prefDirection1)) {
+                currentDirection = prefDirection1;
+            }
+            else if (canMove(prefDirection2)) {
+                currentDirection = prefDirection2;
+            }
+            else while(!canMove(currentDirection) && tries < 10) {
+                    currentDirection = random(Model.MIN_DIRECTION, Model.MAX_DIRECTION);
+                    tries ++;
+                }
         }
-        return random(Model.MIN_DIRECTION, Model.MAX_DIRECTION);
+        return currentDirection;
     }
 }
 
